dec168f93dbd3a033cd3c87dd92beea6405da27d
Lorena De Armas <lorenadearmas01@gmail.com>
2023-09-30 11:13:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container  mx-auto flex flex-col'>
+      <header>
+          <h1>hola</h1>
+      </header>
+      <main>
+          <h2>main</h2>
+      </main>
+      <footer>
+          <h3>footer</h3>
+      </footer>
+    </div>
+  )
+}
+
+export default App
