cb7e301da7e4ec48dca595fb893cd00482bb5ed3
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-14 20:37:31 -0300

    projects-reducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROJECTS, GET_PROJECTS_BY_ID } from "../actions/projects/ationTypes";
+import { GET_PROJECTS, GET_PROJECTS_BY_ID, POST_PROJECT, PUT_PROJECT } from "../actions/projects/ationTypes";
 
 const initalState = {
     projects: [],
@@ -17,8 +17,25 @@ export function projectReducer(state=initalState, action) {
             return {
                 ...state,
                 detailProject: action.payload
-            }
+            };
 
+        case POST_PROJECT:
+            return {
+                ...state,
+            };
+
+        case PUT_PROJECT:
+            return {
+                ...state,
+                detailProject: action.payload
+            };
+
+        case DELETE_PROJECT:
+            const projectsFiltered = state.projects.filter(p => p.id !== action.payload);
+            return {
+                ...state,
+                projects: projectsFiltered
+            }
 
         default:
             return state;
