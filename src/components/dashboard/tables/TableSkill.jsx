c31b8a316e51c8c07a11f27ecc3df56219529c5f
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-19 17:30:27 -0300

    table-show-skills

---
+++ src/components/dashboard/tables/TableSkill.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import './Projects.scss';
+import { useSelector } from 'react-redux';
+import { Fragment } from 'react';
+
+const TableSkill = () => {
+    const skills = useSelector(state => state.skills.skills);
+    console.log('skills :>> ', skills);
+
+
+  return (
+    <div className='accordion' id='accordionSkill'>
+        {/* Show Skill */}
+        <div className="accordion-item">
+            <h2 className="accordion-header" id='headingOne'>
+                <button className="accordion-button"
+                    type='button'
+                    data-bs-toggle='collapse'
+                    data-bs-target='#showSkill'
+                    aria-expanded='false'
+                    aria-controls='collapseOne'
+                >
+                    SHOW SKILL
+                </button>
+            </h2>
+            <div id='showSkill'
+                className="accordion-collapse collapse"
+                aria-labelledby='headingOne'
+                data-bs-parent='#accordionSkill'
+            >
+                <div className="accordion-body">
+                    <div className='attributes'>
+                        <table className="table table-group-divider">
+                            <thead>
+                                <tr>
+                                    <th scope='col'>ID</th>
+                                    <th scope='col'>Image</th>
+                                    <th scope='col'>Skill</th>
+                                    <th scope='col'>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    skills && skills.map(s => (
+                                        <Fragment key={s.id}>
+                                            <tr>
+                                                <th scope='row'>{s.id}</th>
+                                                <td><img src="" alt={s.name} /></td>
+                                                <td>{s.name}</td>
+                                                <td>
+                                                    <button className="btn btn-light">
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            width="15"
+                                                            height="15"
+                                                            fill="currentColor"
+                                                            className="bi bi-trash"
+                                                            viewBox="0 0 16 16"
+                                                        >
+                                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                            <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                                        </svg>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </Fragment>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* Add Skill */}
+        <div className="accordion-item">
+            <h2 className="accordion-header" id='headingTwo'>
+                <button className="accordion-button">
+                    
+                </button>
+            </h2>
+        </div>
+      
+    </div>
+  )
+}
+
+export default TableSkill
