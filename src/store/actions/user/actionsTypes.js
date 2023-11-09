551326ec0e3a5bbf45b6700f382becae3dfedcfe
Lorena De Armas <lorenadearmas01@gmail.com>
2023-11-09 16:17:05 -0300

    actions-user

---
+++ src/store/actions/user/actionsTypes.js
@@ -0,0 +1,5 @@
+export const GET_USERS = 'GET_USERS';
+export const DELETE_USER = 'DELETE_USER';
+
+export const SIGNIN_USER = 'SIGNIN_USER';
+export const SIGNOUT_USER = 'SIGNOUT_USER';
---
+++ src/store/actions/user/index.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import SERVER from '../../../server/index'
+import { DELETE_USER, GET_USERS, SIGNIN_USER, SIGNOUT_USER } from './actionsTypes';
+
+
+export const getUser = () => async(dispatch) => {
+    try {
+        const { data } = await axios.get(`${SERVER}/user`);
+        dispatch({
+            type: GET_USERS,
+            payload: data
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteUser = (id) => async(dispatch) => {
+    try {
+        const { data } = await axios.delete(`${SERVER}/user/${id}`);
+        dispatch({
+            type: DELETE_USER,
+            payload: data
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const signin = (email, password) => async(dispatch) => {
+    try {
+        const { data } = await axios.post(`${SERVER}/user/signin`, {email, password});
+        dispatch({
+            type: SIGNIN_USER,
+            payload: data
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const signout = () => {
+    dispatch({
+        type: SIGNOUT_USER,
+    });
+    window.localStorage.clear();
+}
